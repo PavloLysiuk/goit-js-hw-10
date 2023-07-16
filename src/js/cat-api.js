efca2c7843d075465c2e4af18ac5aa8692784886
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-17 01:53:37 +0300

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,9 +1,27 @@
+// import axios from "axios";
+
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_TzQzII2mDrBqeghPsCtH5cSfSuyDQcdvgcesZiueTT55HV5mo2EOQ98nYpxb5FnF';
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
-  fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+  const END_POINT = '/breeds';
+  fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  const END_POINT = '/images/search?breed_ids=${breedId}';
+  fetch(`${BASE_URL}${END_POINT}&api_key=${API_KEY}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.statusText);
@@ -17,6 +35,6 @@ function fetchBreeds() {
     });
 }
 
-function fetchCatByBreed(breedId) {}
-
 export { fetchBreeds, fetchCatByBreed };
+
+fetchCatByBreed('abys')
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
-// new SlimSelect({
-//   select: '.breed-select',
-// });
+import 'slim-select/dist/slimselect.css';
 
 const selectors = {
   select: document.querySelector('.breed-select'),
@@ -12,8 +10,37 @@ const selectors = {
   catInfo: document.querySelector('.cat-info'),
 };
 
-// selectors.select.addEventListener('change', onSelect);
+selectors.error.hidden = true;
+selectors.catInfo.hidden = true;
 
-// function onSelect(e) {}
+// fetchBreeds()
+//   .then(data => {
+//     const markup = data
+//       .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+//       .join('');
+//     selectors.select.innerHTML = markup;
+//     new SlimSelect({
+//       select: '.breed-select',
+//     });
+//     selectors.loader.classList.replace('loader', 'is-hidden');
+//   })
+//   .catch(onError());
 
-fetchBreeds();
+// selectors.select.addEventListener('change', onSelectBreed);
+
+// function onSelectBreed(e) {}
+
+function onError() {
+  selectors.select.classList.remove('is-hidden');
+  selectors.loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      position: 'center-center',
+      timeout: 5000,
+      width: '400px',
+      fontSize: '24px',
+    }
+  );
+}
