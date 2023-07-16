9c65c834ac868d3be3166c64a0f44ceae1b78306
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-16 18:03:04 +0300

    slimSelect added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+new SlimSelect({
+  select: '.breed-select',
+});
 
 const selectors = {
   select: document.querySelector('.breed-select'),
@@ -7,3 +11,5 @@ const selectors = {
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
+
+selectors.select.addEventListener('change', onSelect);
